d5b71266f83167c52acb2a63609a99cd2623c1e3
Andrii Kr <akryglui13@gmail.com>
2018-03-16 23:29:58 +0200

    Initial commit

---
+++ index.php
@@ -0,0 +1,172 @@
+<html>
+<head>
+<title>Berserk</title>
+<link type="text/css" href="site.css" rel="stylesheet">
+</head>
+<body style="margin:0px;">
+
+	<!-- main page -->
+	<div class="main-frame background-cover">
+		<div class="header">		
+			<div class="left freespace"></div>
+			<div class="header-links left">
+				<div class="header-links-left hover-underscore"><a href="#" >Про нас</a></div>
+				<div class="header-links-left hover-underscore"><a href="#" >Події</a></div>
+				<div class="header-links-left hover-underscore"><a href="#" >Заходи</a></div>
+				<div class="header-links-left hover-underscore"><a href="#" >Тренування</a></div>
+				<div class="header-links-left hover-underscore"><a href="#" >Контакти</a></div>
+			</div>			
+			<div class="freespace right"></div>
+			<div class="header-links right" style="width:25%;">
+				<div class="header-links-right"><span class="right">+38 (050) 131 83 03</span></div>
+				<div class="header-links-right"><span href="#" class="right">+38 (050) 664 25 94</span></div>
+				<div class="header-links-right right">
+					<div class="language hover-underscore left" ><a href="#" class="active-link" >ua</a></div>
+					<div class="language hover-underscore left" ><a href="#" >en</a></div>
+				</div>
+			</div>
+			<center>
+				<div class="header-logo">
+					<img src="img/logo.svg" >
+				</div>
+			</center>
+		</div>
+		<div class="main-page-text">Історія ближче, ніж ти думаеш</div>
+		<center><img class="arrow-down" src="img/down_arrow.png"/></center>
+		<div class="horizontal-line">
+			<div class="back-change-item active"></div>
+			<div class="back-change-item"></div>
+			<div class="back-change-item"></div>
+			<div class="main-horizontal-line"></div>
+		</div>
+	</div>
+	
+	<!-- about page -->
+	<div class="about-frame background-cover">
+		<div>
+			<center>
+			<div class="header-text">ПРО НАС</div>
+			<img src="img/decor-line.png"/>
+			<div class="content-text">КІР БЕРСЕРК  (Чернівецька міська молодіжна громадська 
+			організація Клуб історичної реконструкції «Берсерк) 
+			заснований у 2010 році Андрієм та Максом, які 
+			захоплювались історією могутніх воїнів - вікінгів.</div>
+			<a href="#" class="btn"><span class="icon icon-lines"></span>Читати далі</a>
+			</center>
+		</div>
+	</div>
+	
+	<!-- about page photos -->
+	<div class="about-photos-frame">
+		<div class="left photos-frame" >
+			<div class="about-1 background-cover left"></div>
+			<div class="about-2 background-cover left"></div>
+			<div class="about-4 background-cover left"></div>
+		</div>
+		<div class="about-3 background-cover left"></div>		
+	</div>
+	
+	<!-- podii page -->
+	<div class="podii-frame background-cover">
+		<div>
+			<center>
+			<div class="header-text">ПОДІЇ</div>
+			<img src="img/decor-line.png"/>
+			<div class="content-text">
+				<b>Основні цілі існування клубу:</b>
+				<br/>
+				- популяризація історії та здорового образу життя в маси, шляхом 
+				створення цікавих проектів та організації масштабних заходів.
+			</div>
+			<div class="podii-icons">				 
+				<div class="left podii-icon">
+					<center>
+					<img src="img/01.svg"/><br/><br/><br/>
+					<span class="podii-elem-h">Фестиваль Мідсоммар</span><br/><br/>
+					<span class="podii-elem-text">Міжнародний фестиваль історичної реконструкції слов’яно-скандинавської культури та побуту 9-11 століття</span>
+					</center>
+				</div>
+				<div class="left podii-icon">
+					<img src="img/03.svg"/><br/><br/><br/>
+					<span class="podii-elem-h">«Рагнарок. Початок легенди»</span><br/><br/>
+					<span class="podii-elem-text">Театралізована вистава</span>
+				</div>
+				<div class="left podii-icon">
+					<img src="img/02.svg"/><br/><br/><br/>
+					<span class="podii-elem-h">«Беовульф та Грендель»</span><br/><br/>
+					<span class="podii-elem-text">Театралізована вистава</span>
+				</div>
+				<div class="left podii-icon">
+					<img src="img/04.svg"/><br/><br/><br/>
+					<span class="podii-elem-h">«Скарби Нібелунгів»</span><br/><br/>
+					<span class="podii-elem-text">Театралізована вистава</span>
+				</div>				
+			</div>
+			</center>
+		</div>
+	</div>
+
+	<!-- zahodi page -->
+	<div class="zahodi-frame background-cover">
+		<div class="zahodi-content">
+				<div class="header-text" >ЗАХОДИ</div>
+				<img src="img/decor-line-light.png"/>
+				<div class="content-text">Ми обожнюємо неповторну атмосферу раннього середньовіччя. 
+					Дзвін мечів та дух історії, запал битви та яскраві костюми, запальні 
+					танці та романтизм життя, захоплюючі забави та сурові ...</div>				
+		</div>
+		<div class="zahodi-button"><a href="#" class="btn btn-white"><span class="icon icon-lines"></span>Читати далі</a></div>
+		<div class="zahodi-icon"><img src="img/play.svg"></div>
+	<!-- <iframe style="min-width: 100%;min-height: 100%;" src="https://www.youtube.com/embed/eCt2wPxTgfE?rel=0&amp;controls=0&amp;showinfo=0&amp;autoplay=0" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe> -->
+	<!-- <iframe style="min-width: 100%;min-height: 100%;" src="https://www.youtube.com/embed/tgbNymZ7vqY?playlist=tgbNymZ7vqY&loop=1"></iframe> -->
+		<!-- <video autoplay  loop class="background-video">
+			<source src="img/back_video.avi">
+		</video> -->
+	</div>
+	
+	<!-- trenuvana page -->
+	<div class="tren-frame">
+		<div class="tren-text-contain left">
+			<div class="tren-1 background-cover"></div>
+			<div style="height:50%;">
+			<div class="left tren-freespace"></div>
+			<div class="left">
+				<div class="header-text">Тренування</div>
+				<img src="img/decor-line-light.png"/>
+				<div class="content-text">Спортивне історичне фехтування — це місце 
+				народження майбутніх бійців, які можуть 
+				продемонструвати свої навики на фестивалях 
+				та різних змаганнях ...</div>
+				<a href="#" class="btn btn-white"><span class="icon icon-lines"></span>Читати далі</a>
+			</div>
+			</div>
+		</div>
+		<div class="tren-contain left" >
+			<div class="tren-2 background-cover left"></div>
+			<div class="tren-3 background-cover left"></div>
+			<div class="tren-4 background-cover left"></div>
+		</div>
+	</div>
+	
+	<!-- contacts page -->
+	<div class="contacts-frame background-cover">
+		<center>
+		<div class="contact-head">залишились запитання?</div>
+		<div class="contact-text">Заповніть і ми обов’язково з вами з’яжемось :)</div>
+		<form class="contact-form">
+			<input type="text" class="form-input small left" placeholder="ВАШЕ ІМ'Я"/>
+			<input type="text" class="form-input small right" placeholder="EMAIL"/><br/>
+			<textarea  class="form-input big" placeholder="ПОВІДОМЛЕННЯ..."></textarea>
+			<a onclick="form.submit();" class="btn"><span class="icon icon-lines"></span>Відправити</a>				
+		</form>
+		<div class="lineup"><hr/></div>
+		<div class="lineup"><a href="#" class="social vk"> </a><a href="#" class="social in"> </a><a href="#" class="social fb"> </a></div>
+		<div class="lineup"><hr/></div>
+		</center>
+	</div>
+	<footer class="site-footer">
+		<span class="footertext left">© 2018 berserk. All rights reserved</span>
+		<span class="footertext right">Made by T&T</span>
+	</footer>
+</body>
+</html>
